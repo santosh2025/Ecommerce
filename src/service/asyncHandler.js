abe47365b3ec4bf5ca0b944ab2add9b707ce8131
santosh2025 <100549665+santosh2025@users.noreply.github.com>
2024-06-21 13:56:47 +0000

    Higer-order function

---
+++ src/service/asyncHandler.js
@@ -0,0 +1,24 @@
+
+// const asyncHandler = "Santosh"
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => {}
+//const asyncHandler = (func) => () => {}
+//const asyncHandler = (func) => async(req,res,next) => {}
+// function gives output as req , res and next , that i am imputing to asyn function
+// req is from frontend 
+// res comming from server 
+// next is middleware
+
+const asyncHandler = (fn) => async (req,res,next) => {
+       try {
+            await fn(req,res,next)
+       } catch (error) {
+             res.status(error.code|| 500).json({
+                sucess : false,
+                message : error.message
+             })            
+       }
+}   
+
+
+export default asyncHandler;
