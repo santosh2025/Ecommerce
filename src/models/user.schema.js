250ea3d26ab7a891accf4affa18f38fccc2e1135
santosh2025 <100549665+santosh2025@users.noreply.github.com>
2024-06-22 14:24:09 +0000

    middleware

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -53,7 +53,7 @@ userSchema.methods = {
     // u can now rome around the websitegenerate JWT token
 
     getJWTtoken : function(){
-        JWT.sign({_id: this._id} , config.JWT_SECRET,{
+        JWT.sign({_id: this._id , role : this.role} , config.JWT_SECRET,{
             expiresIn : config.JWT_EXPIRY
         })
     },
